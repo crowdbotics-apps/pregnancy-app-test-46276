53cc6370d7a67f34043afee2d7a1a98861ed07cb
Crowdbotics <team@crowdbotics.com>
2024-02-12 17:19:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pregnancy-app-test-46276",
+  url: "https://pregnancy-app-test-46276.botics.co",
+  api: "https://pregnancy-app-test-46276.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
